0d6e67845dc4dc29fc6782736a1cd068b3d47c67
Daksh-Op21 <gamerdaksh19@gmail.com>
2024-07-22 15:35:26 +0530

    Create users_chat_db.py

---
+++ database/users_chat_db.py
@@ -0,0 +1,38 @@
+import motor.motor_asyncio
+from info import DATABASE_NAME, DATABASE_URI, PROTECT_CONTENT
+
+class Database:
+
+    async def remove_ban(self, id):
+        ban_status = dict(
+            is_banned=False,
+            ban_reason=''
+        )
+        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
+    
+    async def ban_user(self, user_id, ban_reason="No Reason"):
+        ban_status = dict(
+            is_banned=True,
+            ban_reason=ban_reason
+        )
+        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
+
+    async def get_ban_status(self, id):
+        default = dict(
+            is_banned=False,
+            ban_reason=''
+        )
+        user = await self.col.find_one({'id':int(id)})
+        if not user:
+            return default
+        return user.get('ban_status', default)
+
+    async def get_banned(self):
+        users = self.col.find({'ban_status.is_banned': True})
+        chats = self.grp.find({'chat_status.is_disabled': True})
+        b_chats = [chat['id'] async for chat in chats]
+        b_users = [user['id'] async for user in users]
+        return b_users, b_chats
+
+      
+db = Database(DATABASE_URI, DATABASE_NAME)
